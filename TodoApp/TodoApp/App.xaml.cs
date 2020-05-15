cdb118ebf51499dbc5c704a836b3e6a588c0349a
deepjain73 <38891452+deepjain73@users.noreply.github.com>
2020-05-14 18:08:59 -0700

    Add project files.

---
+++ TodoApp/TodoApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoApp
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            FolderPath = Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData);
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
